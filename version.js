d0328e3fff22bc6b9c3266d9687d5dfd9d5f9fb1
jorper98 <jorper98@gmail.com>
2025-09-23 15:41:32 -0700

    Update version.js
    Move Debug Functions

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 {
-    "version": "1.0.3",
+    "version": "1.0.4",
     "changelog": "Fixed repeated admin notice - now shows only once",
     "required_wp_version": "5.0",
     "tested_wp_version": "6.4"
