e6f053b331560fd08a0de39d734cea0486e2c86f
jorper98 <jorper98@gmail.com>
2025-09-23 15:27:50 -0700

    Update version.js

--- version.js
+++ version.js
@@ -1,5 +1,6 @@
 {
-    "version": "1.0.1",
-    "changelog": "Updated from GitHub",
-    "required_wp_version": "5.0"
-  }
+    "version": "1.0.3",
+    "changelog": "Fixed repeated admin notice - now shows only once",
+    "required_wp_version": "5.0",
+    "tested_wp_version": "6.4"
+}
