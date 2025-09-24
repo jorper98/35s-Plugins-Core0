5870dde1c90d26d1d5d66e6d848788af0b30afc7
jorper98 <jorper98@gmail.com>
2025-09-23 19:11:17 -0700

    Update version.js
    Misc Minor Bug Changes

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 {
-    "version": "1.0.4",
-    "changelog": "Fixed repeated admin notice - now shows only once",
+    "version": "1.0.5",
+    "changelog": "Misc bug fixes",
     "required_wp_version": "5.0",
     "tested_wp_version": "6.4"
 }
